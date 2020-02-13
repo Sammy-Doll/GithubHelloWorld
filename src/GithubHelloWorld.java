2aabfc23985053c4e794d2dcec36ee7ee1ecf922
Sammy Doll <samuel.doll.2023@student.lhschools.org>
2020-02-13 13:39:36 -0500

    printedNormalHelloWorld

--- src/GithubHelloWorld.java
+++ src/GithubHelloWorld.java
@@ -2,17 +2,10 @@ public class GithubHelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         helloWorld();
-        yourName();
     }
 
     public static void helloWorld(){
-        System.out.println("Hello Github World");
+
     }
-
-
-    public static void yourName(){
-        System.out.println("This is Sammy");
-    }
-
 
 }
